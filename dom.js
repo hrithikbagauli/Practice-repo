65c4cbf72cde0c90c302530477f96e9a52dc4bb5
Hrithik Bagauli <hrithikbagauli@gmail.com>
2022-09-08 14:03:05 +0530

    commiting files for task 3 sharpener

---
+++ dom.js
@@ -0,0 +1,25 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 'hello';
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[1]);
+// // document.all[10].textContent = 'hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.forms[0]);
+// console.log(document.images);
+
+//console.log(document.getElementById('header-title'));
+var header = document.getElementById('header-title');
+// header.textContent = 'hello';
+// header.innerText = 'bye';
+
+// header.innerHTML = '<h1> hello </h1>'
+header.style.borderBottom = '4px solid black';
+document.querySelector('.title').style.color = 'green';
+document.querySelector('.title').style.fontWeight = 'bold';
