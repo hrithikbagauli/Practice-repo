0f99d6269b55bdee13c7b973aa994175ac399049
Hrithik Bagauli <hrithikbagauli@gmail.com>
2022-10-01 21:11:18 +0530

    storing things on a cloud storage(crudcrud) using post

--- GitStart/test1.js
+++
@@ -1,5 +0,0 @@
-const promise1 = new Promise(()=>{
-    console.log('gello')
-}) 
-
-promise1.then();
--- practice.js
+++ practice.js
@@ -1,158 +1,58 @@
-// console.log('person1: shows ticket');
-// console.log('person2: shows ticket');
+const myform = document.getElementById('myform');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const div = document.getElementById('error');
+const ul = document.getElementById('items');
 
-// const promiseWifeBringingTickets = new Promise((resolve, reject)=>{
-//     setTimeout(() => {
-//         resolve('ticket');
-//     }, 3000);
-// })
+document.addEventListener('DOMContentLoaded',function(){
+    axios.get('https://crudcrud.com/api/a190194a41a4439184b3851c5d01ffee/loginDetails').then((res)=>{
+        res.data.forEach((i) => {
+            const delbtn = document.createElement('button');
+            delbtn.appendChild(document.createTextNode('Delete'));
+            const editbtn = document.createElement('button');   
+            editbtn.appendChild(document.createTextNode('Edit'));
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(i.name+" - "+i.email+" "));
+            li.appendChild(delbtn);
+            li.appendChild(editbtn);
+            ul.appendChild(li);
+            delbtn.addEventListener('click',function(e){
+                e.preventDefault();
+                ul.removeChild(e.target.parentElement);
+            })
+        });
+    }
+    ).catch(err=>console.log(err));
+})
 
+myform.addEventListener('submit', function(e){
+    e.preventDefault();
+    const li = document.createElement('li');
+    const text = document.createTextNode(username.value+" - "+email.value+"  ");
+    const delbtn = document.createElement('button');
+    const editbtn = document.createElement('button');   
 
-// const getPopcorn = promiseWifeBringingTickets.then((t)=>{
-//     console.log('wife: i have the tickets');
-//     console.log('husband: we should go in');
-//     console.log('wife: no i am hungry');
-//     return new Promise((resolve, reject) => resolve(`${t} popcorn`))
-// });
+    if(username.value == '' || email.value == ''){
+        div.appendChild(document.createTextNode('Please fill both the fields'));
+        setTimeout(() => {
+            div.removeChild(div.firstChild);
+        }, 3000);
+    }
+    else{
+        delbtn.appendChild(document.createTextNode('Delete'));
+        editbtn.appendChild(document.createTextNode('Edit'));
+        li.appendChild(text);
+        li.appendChild(delbtn);
+        li.appendChild(editbtn);
+        ul.appendChild(li);
+        axios.post('https://crudcrud.com/api/a190194a41a4439184b3851c5d01ffee/loginDetails',{name: username.value, email: email.value}).catch(err=>console.log('something went wrong'));
+    }
 
-// const getButter = getPopcorn.then((t)=>{
-//     console.log('husband: i got some popcorn');
-//     console.log('wife: we should go in');
-//     console.log('wife: i need butter on my popcorn');
-//     return new Promise((resolve, reject) => resolve(`${t} butter`))
-// });
+    delbtn.addEventListener('click',function(e){
+        e.preventDefault();
+        ul.removeChild(e.target.parentElement);
+    })
+})
 
-// const getColdDrinks = getButter.then((t)=>{
-//     console.log('husband: i got some butter');
-//     console.log('wife: lets get something to drink as well');
-//     return new Promise((resolve, reject) => resolve(`${t} cold drinks`))    
-// });
-
-// getColdDrinks.then((t)=>{
-//     console.log(t);
-// })
-
-// console.log('person4: shows ticket');
-// console.log('person5: shows ticket');
-
-
-
-
-
-
-
-// console.log('person1: shows ticket');
-// console.log('person2: shows ticket');
-
-// const preMovie = async()=>{
-
-//     const promiseWifeBringingTickets = new Promise((resolve, reject)=>{
-//         setTimeout(() => {
-//             resolve('tickets');
-//         }, 3000);
-//     })
-
-//     const getPopcorn = new Promise((resolve, reject)=> resolve('popcorn'));
-//     const getButter = new Promise((resolve, reject)=> resolve('butter'));
-//     const getColdDrinks = new Promise((resolve, reject)=> resolve('cold drinks'));
-
-//     let ticket = await promiseWifeBringingTickets;
-
-//     console.log(`wife: i have the ${ticket}`);
-//     console.log('husband: we should go in');
-//     console.log(`wife: no i am hungry`);
-
-//     let popcorn = await getPopcorn;
-
-//     console.log(`husband: i got some ${popcorn}`);
-//     console.log('husband: we should go in');
-//     console.log('wife: i need butter on my popcorn');
-
-//     let butter = await getButter;
-
-//     console.log(`husband: i got some ${butter}`);
-
-//     let colddrink = await getColdDrinks;
-
-//     console.log(`husband: i got the ${colddrink}`);
-//     console.log(`husband: anything else?`);
-//     console.log(`wife: lets go, we're getting late`);
-//     console.log(`husband: thanks for the reminder -_-`);
-
-//     return ticket;
-// }
-
-// preMovie().then((t)=>console.log('person3: shows the '+t));
-
-// console.log('person4: shows ticket');
-// console.log('person5: shows ticket');
-
-
-// const posts = [{title: 'post one', body: 'this is post one'}, {title: 'post two', body: 'this is post two'}];
-
-// const myfunc = async ()=>{
-//     function displayPosts(){
-//         let output = '';
-//         posts.forEach((post)=>{
-//             output = output + `<li> ${post.title} </li>`;
-//         })
-//         document.body.innerHTML = output;
-//     }
-    
-//     function createPost(post){
-//         return new Promise((resolve, reject)=>{
-//             setTimeout(() => {
-//                 if(posts.push(post)){
-//                     resolve();
-//                 }
-//                 else{
-//                     reject();
-//                 }
-//             }, 1000);
-//         });
-//     }
-    
-//     function deletePost(){
-//         return new Promise((resolve, reject)=>{
-//             setTimeout(() => {
-//                 if(posts.pop())
-//                     resolve();
-//                 else{
-//                     reject();
-//                 }
-//             }, 1000);
-//         })
-//     }
-
-//     displayPosts();
-//     await createPost({title: 'post three', body: 'this is post three'});
-//     displayPosts();
-//     await createPost({title: 'post four', body: 'this is post four'});
-//     displayPosts();
-//     await deletePost();
-//     displayPosts();
-//     await deletePost();
-//     displayPosts();
-//     await deletePost();
-//     displayPosts();
-//     await deletePost();
-//     displayPosts();
-
-// }
-
-// myfunc();
-
-
-
-//Why were promises discovered? promises were created as a better alternative to callback functions. Using callback functions one inside another inside another and so on can lead to something called as callback hell. Promises make the code more readable and easier to manage. In addition to that, promises also have better error handling because of the .catch() method.
-
-//async await makes the code more understandable and easy to manage. It allows us to write asynchronous code which looks like synchronous code, hence, making it easier to read and understand. e.g.
-//In the code at the top, using .then() multiple times makes the code look very complicated and hard to understand. On the other hand, the same functionalities were achieved using the await keyword and the code is also quite easy to understand.
-
-
-
-
-
-
-
-
+// axios.get('https://crudcrud.com/api/c57d7a3abb244e5da078da0599a82e5d/loginDetails').then(res=>console.log(res.data)).catch(err=>console.log('something went wrong'));
+// axios.delete('https://crudcrud.com/api/a190194a41a4439184b3851c5d01ffee/loginDetails/6337e84506e25f03e8c5dec4');
