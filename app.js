d19de580608dd487129b4179673a1412180bc388
hrithikbagauli <68984488+hrithikbagauli@users.noreply.github.com>
2022-10-16 16:16:06 +0530

    redirecting to the same page after doing a post request

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 const http = require('http');
-//deliverable no. 1
-// const server = http.createServer((req, res)=>{
-//     res.setHeader('Content-Type', 'text/html');
-    // res.write('<html>');
-    // res.write('<head><title>my page</title></head>');
-    // res.write('<body><h1>Welcome to my Node JS project!</h1> </body>')
-    // res.write('</html>');
-    // res.end();
-// });
+const fs = require('fs');
 
-//deliverable no. 2
 const server = http.createServer((req,res)=>{
     const url = req.url;
-    if(url=='/home'){
+    const method = req.method;
+    
+    if(url==='/'){ //=== checks if the url is a string and also has the correct value i.e. '/' in this case.
         res.write('<html>');
-        res.write('<head><title>my page</title></head>');
-        res.write('<body><h1>Welcome home!</h1> </body>')
+        res.write('<head><title>enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>') //here, the request that is automatically generated for the post request is directly sent to the url mentioned in form action, which in this case is '/message' which basically means 'localhost:4000/message'.we're using post as the method here because we want to send data to another url. so this will send a post request to the mentioned url and it'll also look into the input fields and check if there's any input. The name given to input is later used a key to access the input.
         res.write('</html>');
+        return res.end(); //using return here so that the code below doesn't get executed when this if condition runs.
     }
-    else if(url=='/about'){
-        res.write('<html>');
-        res.write('<head><title>my page</title></head>');
-        res.write('<body><h1>Welcome to about us!</h1> </body>')
-        res.write('</html>');
+    if(url==='/message' && method==='POST'){
+        fs.writeFileSync('message.txt', 'dummy data'); //creating a new file called message.txt with content as 'dummy data'.
+        res.statusCode = 302;   //setting the status code as 302 because its the status code for redirection.
+        res.setHeader('Location', '/'); //setting the header's location to '/' so that it redirects the page to '/' which is the root directory.
+        return res.end();
     }
-    else if(url=='/node'){
-        res.write('<html>');
-        res.write('<head><title>my page</title></head>');
-        res.write('<body><h1>Welcome to my Node JS project!</h1> </body>')
-        res.write('</html>');
-    }
-    res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>message page</title></head>');
+    res.write('<body><h1>welcome to my node js server!</h1></body>')
+    res.write('</html>');
+    return res.end();
 });
 
 server.listen(4000);
