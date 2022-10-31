a007e327a15c3b5f33346a345004faaadc59ee04
hrithikbagauli <68984488+hrithikbagauli@users.noreply.github.com>
2022-10-31 09:10:18 +0530

    sending html pages using res.sendFile()

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
 const express = require('express');
-const app = express();
-const bodyparser = require('body-parser');
-const fs = require('fs');
+const adminRoutes = require('./routes/admin'); 
+const shopRoutes = require('./routes/shop');  
+const path = require('path');
 
-app.use(bodyparser.urlencoded({extended:false}));
+const bodyparser = require('body-parser'); 
 
-app.post('/', (req, res, next)=>{
-    fs.writeFile('message.txt', `${req.body.username} : ${req.body.message} `, {flag: 'a'}, (err)=>{ //flag: 'a' sets the file mode to 'append' which means any new data will be appended to the existing data and not replace the previous data. The default mode is to replace the data when you write in a file.
-        err? console.log(err):res.redirect('/');
-    })
+const app = express(); 
+
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use(adminRoutes); 
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{ 
+    res.status(404).sendFile(
+        path.join(__dirname, 'views', 'notfound.html')
+    )
 })
 
-app.get('/', (req, res, next)=>{
-    fs.readFile('message.txt', {encoding: 'utf-8'}, (err, data)=>{//data is the data that has been read from the file.
-        if(err){//an error will occur if there's no data to read. So, we're using that for a condition. In case of an error, set the value of data to 'no chats exist'
-           data = 'no chats exist';
-        }
-        //creating a hidden input field so that it doesn't appear on the webpage. This is just a dummy input field.
-        //taking the value from the hidden dummy field and setting its value to the username stored in the localstorage.
-        res.send(`${data}<form action="/" method="POST" onsubmit="document.getElementById('username').value = localStorage.getItem('username')"> 
-        <input type="text" name="message" placeholder="write a message">
-        <input type="hidden" name="username" id="username"> 
-        <button type="submit">send message</button>
-        </form>`);
-    });
-});
-
-//onsubmit decides what happens when the form is submitted.
-app.get('/login', (req, res, next)=>{
-    res.send(`<form action="/" onsubmit="localStorage.setItem('username', document.getElementById('username').value)"> 
-    <input type="text" id="username" name="username" placeholder="username"><br>
-    <button type="submit">login</button>
-    </form>`);
-})
-
-app.listen(4000);
+app.listen(4000); 
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,11 @@
-const express = require('express'); //importing express 
-const router = express.Router();    //this is how we create a router. This router will allow us to set the logic for each route.
+const express = require('express'); 
+const router = express.Router();  
+const path = require('path'); //importing the path module to work with paths. 
 
 router.get('/addproduct',(req, res, next)=>{
-    res.send('<form action="/admin/addproduct" method="POST"><input type="text" name="title"><select name="size"><option>small</option><option>medium</option><option>large</option></select><button type="submit">add product</button></form>');
-});
-router.post('/addproduct', (req, res, next)=>{ //here, we've used the same path as above because we're using post method here. So, as long as the methods are different we can use the same name for different routes.
-    console.log(req.body); 
-    console.log(req.body.title); 
-    console.log(req.body.size);
-    res.redirect('/'); 
+    res.sendFile(path.join(__dirname, '../', 'views', 'addproduct.html')); //sendFile() allows us to send html pages. We've used path.join() because we cannot directly specify the location of the file we want to send using just '/' or './' because sendFile() assumes that by '/' we mean the root folder of our computer and not the file we're working on. So, we need to specify the absolute path including the computer's root directory. This is what path.join() helps us with. It basically joins the multiple path segments into one and returns one whole path.Here,  __dirname refers to the absolute path of our computer. After __dirname, we have another argument i.e. '../' and here we're telling nodejs to get out of the current folder because we've used sendFile() inside 'admin.js', path will currently point to the 'routes' folder but since our 'views' folder is outside, we need to get out of the 'routes' folder first and that's why we've used '../' here. If we didn't want to get out of the folder, we could've just omitted this argument. In the third argument, since we're out of the 'routes' folder now, here we're specifying which folder to go to i.e. go inside the 'views' folder. Finally, the last argument is the name of the html page.
+    //path.join() basically combines all the paths we've specified and returns one path. e.g. for this case, it returns this :- D:\CODES\sharpenerProjects\GitStart\nodejs\views\shop.html
+    //instead of using '../' we can also use '..' and it'll do the same thing.
 });
 
 module.exports = router;
-
