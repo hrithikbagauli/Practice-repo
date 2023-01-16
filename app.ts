2857c107a31b4888667b4c1b1b60e5cbf32f988d
Hrithik Bagauli <hrithikbagauli@gmail.com>
2023-01-16 14:15:56 +0530

    learning typescript 1

---
+++ app.ts
@@ -0,0 +1,17 @@
+const num1element = document.getElementById('num1') as HTMLInputElement;
+const num2element = document.getElementById('num2') as HTMLInputElement;
+const btn = document.querySelector('button') as HTMLButtonElement;
+
+function add(num1: number, num2: number){
+    return num1 + num2;
+}
+
+add(1, 2);
+btn.addEventListener('click', ()=>{
+    const num1 = num1element.value;
+    const num2 = num2element.value;
+    const result = add(+num1, +num2);
+    console.log(result)
+})
+
+console.log(add(1,6));
